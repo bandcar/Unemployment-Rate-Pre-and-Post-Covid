776d1da26ee9d371c9a0740f5fdffc625af5237d
bandcar <95548481+bandcar@users.noreply.github.com>
2022-04-25 15:35:16 -0500

    Create full_code.R

---
+++ full_code.R
@@ -0,0 +1,27 @@
+library(tidyverse)
+library(rvest)
+# Create url object of website -------------------------------------------------
+url = ("https://www.bls.gov/charts/state-employment-and-unemployment/state-unemployment-rates-animated.htm")
+
+# Change the url to an html object ---------------------------------------------
+page = read_html(url)
+
+# copy the table ---------------------------------------------------------------
+thetable = page %>% 
+  html_element(css= '#lau_rc_unmapanim') %>% 
+  html_table()
+
+# Delete Puerto Rico in row 52, columns 2:59 -----------------------------------
+newtable = thetable[-52, -c(2:59,96:107)]
+
+# Change tibble to data frame --------------------------------------------------
+df <-  data.frame(newtable)
+
+# Get column averages for every year -------------------------------------------
+unemployment = cbind(df[1], sapply(split.default(df[-1], sub("\\D+", "", names(df)[-1])), rowMeans))
+
+# Round numbers to one decimal -------------------------------------------------
+unemployment_rate = unemployment %>% 
+  mutate_if(is.numeric, round, digit = 1)
+
+
