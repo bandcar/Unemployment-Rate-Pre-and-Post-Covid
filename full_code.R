a921fb755a592f3eba577db5b0c52202681e832e
bandcar <95548481+bandcar@users.noreply.github.com>
2022-04-28 02:23:48 -0500

    Update full_code.R

--- full_code.R
+++ full_code.R
@@ -11,23 +11,47 @@ thetable = page %>%
   html_element(css= '#lau_rc_unmapanim') %>% 
   html_table()
 
-# Delete DC and Puerto Rico in rows 9 and 52. Delete columns 2:59 --------------
-newtable = thetable[c(-9,-52), -c(2:59)]
-
-# Change tibble to data frame 
+# Delete DC and Puerto Rico in rows 9 and 52. Delete columns 2:11 --------------
+newtable = thetable[c(-9,-52),-c(2:11)] 
+# Change tibble to data frame --------------------------------------------------
 df <-  data.frame(newtable)
-
 # Change mar and apr 2020 columns to numeric
 df$Mar.2020 <- as.numeric(df$Mar.2020)
 df$Apr.2020 = as.numeric(df$Apr.2020)
 
-# Get column averages for every year -------------------------------------------
+# Add a row for the US average unemployment rate
+df = df %>%
+  ungroup %>%
+  summarise(State = c(State, 'United States'), 
+            across(where(is.numeric), ~ c(., mean(.))))
+
+# Get row averages for every year -------------------------------------------
 unemployment = cbind(df[1], sapply(split.default(df[-1], sub("\\D+", "", names(df)[-1])), rowMeans))
 
 # Round numbers to one decimal -------------------------------------------------
-unemployment_rate = unemployment %>% 
+unemployment = unemployment %>% 
   mutate_if(is.numeric, round, digit = 1)
 
 # pivots years and unemployment rates into columns
-unemployment_rate <- unemployment_rate %>% 
-  pivot_longer(c(`2017`,`2018`,`2019`,`2021`,`2022`), names_to = "Year", values_to = "Rate")
+unemployment_rate <- unemployment %>% 
+  pivot_longer(c(`2013`,`2014`,`2015`,`2016`,`2017`,`2018`,`2019`,`2020`,`2021`,`2022`), names_to = "Year", values_to = "Rate")
+
+# Create CSV for long data------------------------------------------------------
+write.csv(unemployment_rate, "pathname.csv",row.names=FALSE)
+
+q<-read.csv("pathname.csv", stringsAsFactors = F)
+
+save(q, file="pathname.RData")
+
+# Wider version of data
+df2_new = unemployment_rate %>% 
+  pivot_wider(names_from = "State", values_from = "Rate")
+
+# Create CSV of wider data
+write.csv(df2_new, "pathname.csv", row.names = FALSE)
+
+q<-read.csv("pathname.csv", stringsAsFactors = F)
+
+save(q, file="pathname.RData")
+
+
